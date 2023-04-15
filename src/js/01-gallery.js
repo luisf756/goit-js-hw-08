0bc532b6349057767174d25ed4827cd927a7cb87
luisf756 <luchof756@gmail.com>
2023-04-15 16:32:22 -0500

    add firts task and reference

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+// console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const containerGAllery=document.querySelector('.gallery');//ingresar el selector
+const photosMark = createGalleryItem(galleryItems);
+
+function createGalleryItem(element){
+    return element 
+    .map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`
+    })
+    .join('');
+}
+
+
+containerGAllery.insertAdjacentHTML('beforeend',photosMark);
+containerGAllery.addEventListener('click', (event)=>event.preventDefault());
+
+// let lightbox = new SimpleLightbox('.gallery a'); //para utilizar la galeria
+ const galleryHandler = new SimpleLightbox('.gallery a', {
+    captionsData:'alt',
+     captionDelay:250
+    });
+// galleryHandler.on('show.simplelightbox', function (e) {
+//    let img=e.target.childNodes
+//    img[1].alt =img[1].alt + ': comentario'; 
+// });
