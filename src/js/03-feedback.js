54db4c38d77e2d5cf9c32d4e86c718b73ad42d0c
luisf756 <luchof756@gmail.com>
2023-04-17 00:12:02 -0500

    change task 3 and package json

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+// player.on('timeupdate', throttle(() => {
+//     player.getCurrentTime().then((seconds) => {
+//         localStorage.setItem("videoplayer-current-time", seconds)
+//         // console.log(localStorage.getItem("videoplayer-current-time"))
+//     }).catch((error) => {
+//         console.log('Ha ocurrido un error!');
+//     });
+// }, 1000));
+
+const form = document.querySelector(".feedback-form");
+// const output = document.querySelector("#output");
+// const LOCALSTORAGE_KEY = "feedback-form-state";
+const childrens = form.children;
+for (let element of childrens) {
+    if (element.firstElementChild != null) {
+        element.firstElementChild.classList.add(`feedback-form__${element.firstElementChild.getAttribute('name')}`);
+    } else {
+        element.classList.add(`feedback-form__${element.getAttribute('type')}`);
+    };
+
+};
+const [emailSelector, messageSelector, btnSelector] = form.querySelectorAll('.feedback-form__email, .feedback-form__message, .feedback-form__submit');
+
+console.log(childrens)
+// updateOutput();
+// form.addEventListener("submit", saveMessage);
+
+// function saveMessage(evt) {
+//   evt.preventDefault();
+//   localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
+//   updateOutput();
+//   form.reset();
+// }
+
+// function updateOutput() {
+//   output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
+// }
